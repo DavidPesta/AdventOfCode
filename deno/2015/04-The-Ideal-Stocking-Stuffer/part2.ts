42e4c51c82dc2d18f5e577a378e997d163a94a1a
David Pesta <davidpesta@gmail.com>
2022-08-14 16:55:07 -0500

    Simplified md5 hash creation for day 4 of 2015.

--- deno/2015/04-The-Ideal-Stocking-Stuffer/part2.ts
+++ deno/2015/04-The-Ideal-Stocking-Stuffer/part2.ts
@@ -1,20 +1,11 @@
-import { encode } from "https://deno.land/std@0.150.0/encoding/hex.ts";
-import { crypto } from "https://deno.land/std@0.150.0/crypto/mod.ts";
+import easy from "https://deno.land/x/easyutil@0.10.0/mod.ts";
+import { HashAlgorithm } from "https://deno.land/x/easyutil@0.10.0/types.ts";
 
 const input = Deno.readTextFileSync("input.txt");
 
 let i = 0;
 while(true) {
-	const hash = new TextDecoder().decode(
-		encode(
-			new Uint8Array(
-				await crypto.subtle.digest(
-					"MD5",
-					new TextEncoder().encode(`${input}${i}`),
-				),
-			)
-		)
-	);
+	const hash = await easy.cryptography.hash(HashAlgorithm.MD5, `${input}${i}`);
 	
 	if (hash.slice(0, 6) == "000000") {
 		console.log(i);
